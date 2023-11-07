3f70d68cf82d028089f424a91bfb1912a3646496
ErickFlorecz <erickflorecz@gmail.com>
2023-11-07 02:33:09 -0600

    Personalización de desarrollo

--- Frontend/marketplace/src/App.tsx
+++ Frontend/marketplace/src/App.tsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      Marketplace
+      <div className=''>
+        {/* <div className="flex justify-center">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+            <img src={reactLogo} className="logo react" alt="React logo" />
+        </div> */}
+      <div className="text-right">
+      <h1 className='font-bold'>IndustryXTrade</h1>
+      <p>powered by <strong>Alfil Tecnologia</strong></p>
+      </div>
+      </div>
+      <div className="relative -bottom-12">
+        <p className='text-2xl'>En desarrollo </p>
+      </div>
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div> */}
     </>
   )
 }
